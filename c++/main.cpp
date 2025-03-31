0dce1f501628a6ab1b9e908d9b43103157a33a9e
Sachin <sk9006038@gmail.com>
2025-03-31 18:21:26 +0530

    linkedlist

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,9 +1,47 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 
 
-int main(){
-    int a[5] = {1};
-    for(int i = 0; i < 5; i++){
-        printf("%d",a[i]);
+class LinkedListNode{
+    public:
+        int data;
+        LinkedListNode* next;
+    LinkedListNode(int data){
+        this -> data = data;
+        this -> next = NULL;
     }
+
+
+};
+
+void InsertAtHead(LinkedListNode* &tail,int d){
+    // new node create
+    LinkedListNode* temp = new LinkedListNode(d);
+    temp -> next = tail;
+    tail = temp;
+
+}
+
+void print(LinkedListNode* &tail){
+    LinkedListNode* temp = tail;
+    while (temp != NULL)
+    {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+    
+}
+int main(){
+    LinkedListNode* node1 = new LinkedListNode(10);
+    cout << node1 -> data << endl;
+    cout << node1 -> next << endl;
+    LinkedListNode* tail = node1;
+    print(tail);
+    InsertAtHead(tail,5);
+    InsertAtHead(tail,8);
+    print(tail);
+
+
+    return 0;
 }
